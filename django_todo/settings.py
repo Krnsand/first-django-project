34a34b22859aa250a60723648c3f46a27d0acc65
Krnsand <krnsandin@gmail.com>
2023-03-03 15:19:11 +0000

    Update settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3n$dwgl=++4uk%c7hs$mw+z&yu+)7e28krw%=ab^=rxq%a10(0'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3n$dwgl=++4uk%c7hs$mw+z&yu+)7e28krw%=ab^=rxq%a10(0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['krnsand-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
