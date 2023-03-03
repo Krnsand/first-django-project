e8a5e79c0647aa6a9e865abfd9a32e3cd4fb4fe3
Krnsand <krnsandin@gmail.com>
2023-03-03 14:52:21 +0000

    Fix allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3n$dwgl=++4uk%c7hs$mw+z&yu+)7e28krw%=ab^=rxq%a10(0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['krnsand-django-todo-app.herokuapp.com']
 
 
 # Application definition
